ede1c36d1584269a54f9b8e09bc1d1dd79ade40e
at-prime <karmanya18sharma@gmail.com>
2025-02-09 14:54:25 +0530

    updated2

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
- 
+import React from 'react';
+import { ThemeProvider, createTheme, CssBaseline, Box } from '@mui/material';
+import StockPriceBar from './components/StockPriceBar';
+import './App.css';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1a237e',
+    },
+    secondary: {
+      main: '#0d47a1',
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box className="App">
+        <StockPriceBar />
+        {/* Add other components here */}
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App; 
